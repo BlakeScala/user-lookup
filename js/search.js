1b567b19e9de395f52c2dcbc3c63ae36f8aa7250
Blake Scala <blakescala@gmail.com>
2016-10-14 08:56:10 -0700

    add response div that displays github username, avatar, and link to github page

--- js/search.js
+++ js/search.js
@@ -7,6 +7,9 @@ function Search(){
 Search.prototype.getRepos = function(userName){
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    $('#userName').text(response.login);
+    $('#avatar').attr("src", response.avatar_url);
+    $('#githubLink').attr("href", response.url);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
