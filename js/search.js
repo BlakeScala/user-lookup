7ffd3f787d09218dcc13c0c632c2353381cd02f5
Blake Scala <blakescala@gmail.com>
2016-10-14 12:18:08 -0700

    change ul to ol, fix repository div bug by emptying after each search

--- js/search.js
+++ js/search.js
@@ -7,6 +7,8 @@ function Search(){
 Search.prototype.getRepos = function(userName){
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    $('#failure').hide();
+    $('#allRepositories').empty();
     $('#userPanel').show();
     $('#repositoryPanel').show();
     $('#userName').text(response.login);
@@ -22,9 +24,11 @@ Search.prototype.getRepos = function(userName){
 
   $.get('https://api.github.com/users/' + userName + '/repos?&per_page=1000&access_token=' + apiKey).then(function(response){
     for(var i = 0; i < response.length; i++) {
-      $('#repositoryPanel').append('<a href ="' + response[i].html_url + '">' + '<h3>' +  response[i].name + '</h3>' + '</a>' + '<h5>' + response[i].description + '</h5>');
-      // if (response[i].description === null){
-      // }
+      var description = (response[i].description);
+      if (description === null) {
+        description = "No description";
+      }
+      $('#allRepositories').append('<li><a href ="' + response[i].html_url + '">' + '<h3>' +  response[i].name + '</h3>' + '</a>' + '<h5>' + description + '</h5></li>');
     }
   }).fail(function(error){
     $('#failure').show();
