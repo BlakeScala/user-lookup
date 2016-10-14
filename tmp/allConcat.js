bb75445bbf0cd398a7ccb8dcb74fa026a5ce9f98
Blake Scala <blakescala@gmail.com>
2016-10-14 08:14:50 -0700

    add search bar input and add styling to jumbotron

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,6 +1,11 @@
 var Search = require('./../js/search.js').searchModule;
 
 $(document).ready(function() {
-  userSearch = new Search();
-  userSearch.getRepos();
+  $('#searchBar').submit(function(event){
+    event.preventDefault();
+    var userName = $('#search').val();
+    console.log(userName);
+    userSearch = new Search(userName);
+    userSearch.getRepos(userName);
+  });
 });
